d706b53e4193c7f57994668121a8a735b3a75fd1
Christoph Vitz <c.vitz@itsonix.eu>
2018-02-15 10:19:09 +0100

    RX-12412: Extract send function

--- src/backends/impl-request.js
+++ src/backends/impl-request.js
@@ -2,21 +2,26 @@ import client from 'request-promise'
 
 import { GET } from '../commands/get'
 import RestError from '../error/rest-error'
+import { GET } from '../commands/get'
+
+async function performRequest(method, request) {
+  try {
+    return await client({
+      method,
+      uri: request.path,
+      headers: request.headers,
+      qs: request.query,
+      json: true,
+    })
+  } catch (e) {
+    throw new RestError(e)
+  }
+}
 
 export default () => {
   const backend = {
     async [GET]({ request }) {
-      try {
-        return await client({
-          method: 'GET',
-          uri: request.path,
-          headers: request.headers,
-          qs: request.query,
-          json: true,
-        })
-      } catch (e) {
-        throw new RestError(e)
-      }
+      return performRequest('GET', request)
     },
   }
 
