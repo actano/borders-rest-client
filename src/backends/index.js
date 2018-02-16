82ad49d5932d814443d2f9a278eec0e84c6051e7
Christoph Vitz <c.vitz@itsonix.eu>
2018-02-16 09:49:51 +0100

    RX-12437: Delete `noop` backend
    This backend is not maintained

--- src/backends/index.js
+++ src/backends/index.js
@@ -1,2 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
 export { default as implRequestBackend } from './impl-request'
-export { default as noopBackend } from './noop'
--- test/noop.js
+++
@@ -1,6 +0,0 @@
-import noopBackend from '../src/backends/noop'
-import testBackend from '../src/spec/backend.spec'
-
-describe('borders-rest-client/noop-backend', () => {
-  testBackend(noopBackend)
-})
